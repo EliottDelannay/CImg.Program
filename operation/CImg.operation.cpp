181e9c5d025b27b81489977843a48cd0cc9ae47a
Eliott Delannay <Eliott@delannay.fr>
2020-01-08 11:54:57 +0100

    Operation : create image with baseline

--- operation/CImg.operation.cpp
+++ operation/CImg.operation.cpp
@@ -25,12 +25,30 @@ int main(int argc,char **argv)
   const bool help=cimg_option("-h",false,"display this help.");
   //exit on help request '-h' command line option
   if(help) return 0;
+  //create a image depending of what is chosen ie (baseline, simulation)
+//  const std::string signal=cimg_option("--signal","baseline","image signal either baseline, simulation or files");
   //Load an image
-  const CImg<int> image1(file_i1);
-image1.display_graph("image 1");
+  int baseline = 32;
+  int width = 20;
 
-   	CImg<int> image2(file_i2);
-image2.display_graph("image 2");
+/*
+  if(signal=="baseline")
+  {
+*/
+    CImg<float> image1(width,1,1,1,baseline);
+    image1.display_graph("image 1");
+    CImg<unsigned char> image2 (width,1,1,1,baseline);
+    image2.display_graph("image 2");
+/*
+  }
+  else
+  {
+    const CImg<int> image1(file_i1);
+    image1.display_graph("image 1");
+    const CImg<int> image2(file_i2);
+    image2.display_graph("image 2");
+  }
+*/
 
   if(operation=="maximum")
   {
